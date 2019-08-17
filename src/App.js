ff24ee5dabb618f8810dc025c2d5987a3835207a
Milfren <nerflimjohn@gmail.com>
2019-08-18 00:04:11 +1000

    nav component setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/includes/Nav';
+import Home from './components/pages/home/Home';
 
 function App() {
-	return <div className='App'>Hello World!</div>;
+	return (
+		<div className='App'>
+			<Nav />
+			<Home />
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					lighter: '#48D43C',
+					default: '#0EA800',
+					dark: '#638260'
+				}
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+};
