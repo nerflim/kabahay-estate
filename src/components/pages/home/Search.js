c601d8d34f0c9c8aace183e4de358de88582c189
Milfren <nerflimjohn@gmail.com>
2019-09-15 20:47:02 +1000

    added fontawesome icons

--- src/components/pages/home/Search.js
+++ src/components/pages/home/Search.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
 	const submitHandler = () => {
@@ -6,11 +8,18 @@ const Search = () => {
 	};
 	return (
 		<div className='self-center m-auto container'>
-			<form onSubmit={() => submitHandler()}>
+			<form className='w-8/12 m-auto' onSubmit={() => submitHandler()}>
 				<div className='flex'>
-					<div className='rounded-l-full w-10/12 border-primary border border-r-0 overflow-hidden'>
-						<span className='absolute p-3'>icon</span>
-						<input type='text' className='p-3 pl-12 w-full focus:outline-none' placeholder='Enter a location, project or developer name' />
+					<div className='rounded-l-full w-10/12 border-primary border border-r-0 overflow-hidden text-gray-500'>
+						<span className='absolute py-3 px-5 text-primary'>
+							<FontAwesomeIcon icon={faMapMarkerAlt} />
+						</span>
+
+						<input
+							type='text'
+							className='p-3 pl-12 w-full focus:outline-none focus:text-gray-700'
+							placeholder='Enter a location, project or developer name'
+						/>
 					</div>
 					<div className='w-2/12 bg-white border border-primary border-l-0 rounded-r-full overflow-hidden'>
 						<button type='submit' className='bg-primary text-white p-3 rounded-l-full w-full focus:outline-none'>
