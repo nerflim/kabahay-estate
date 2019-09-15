8505f7a04d524f5f972f2a5d39b5f4389c469d5c
Milfren <nerflimjohn@gmail.com>
2019-09-15 22:26:59 +1000

    added tailwindcss to laravelmix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 
-mix.sass('src/sass/app.scss', 'src/App.css');
+mix.sass('src/sass/app.scss', 'src/App.css').postCss('src/vendor.css', 'src/vendor.compiled.css', [require('tailwindcss')]);
