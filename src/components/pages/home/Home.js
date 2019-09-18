2bb7f08026d1e7f6bcd9b149959113a416cd9a83
Milfren <nerflimjohn@gmail.com>
2019-09-18 22:38:53 +1000

    partners

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Explore from './Explore';
 import LatestNews from './LatestNews';
+import Partners from './Partners';
 
 const Home = () => {
 	return (
@@ -9,6 +10,7 @@ const Home = () => {
 			<Header />
 			<Explore />
 			<LatestNews />
+			<Partners />
 		</div>
 	);
 };
---
+++ src/components/pages/home/Partners.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Partners = () => {
+	return (
+		<div className='bg-gray-200'>
+			<div className='container mx-auto flex items-center h-32'>
+				<div className='w-1/6'>
+					<img src='/img/partners/1.png' alt='AyalaLand' className='h-8 mx-auto' />
+				</div>
+				<div className='w-1/6'>
+					<img src='/img/partners/2.png' alt='Global-Estate Resorts Inc.' className='h-8 mx-auto' />
+				</div>
+				<div className='w-1/6'>
+					<img src='/img/partners/3.png' alt='Suntrust' className='h-8 mx-auto' />
+				</div>
+				<div className='w-1/6'>
+					<img src='/img/partners/4.png' alt='BPI' className='h-8 mx-auto' />
+				</div>
+				<div className='w-1/6'>
+					<img src='/img/partners/5.png' alt='BDO' className='h-8 mx-auto' />
+				</div>
+				<div className='w-1/6'>
+					<img src='/img/partners/6.png' alt='Megaworld' className='h-8 mx-auto' />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Partners;
