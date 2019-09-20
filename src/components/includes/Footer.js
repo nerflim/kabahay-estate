f74334adadbca9dcff56077208dccf47b755438c
Milfren <nerflimjohn@gmail.com>
2019-09-20 16:30:47 +1000

    footer branding

--- src/components/includes/Footer.js
+++ src/components/includes/Footer.js
@@ -16,11 +16,11 @@ const Footer = () => {
 	];
 	return (
 		<footer>
-			<div className='bg-secondary h-64 text-gray-300 text-sm flex items-center'>
+			<div className='bg-secondary py-12 text-gray-300 text-sm flex items-center'>
 				<div className='container mx-auto'>
 					<div className='flex'>
 						{items.map((item, index) => (
-							<div className='w-1/6' key={index}>
+							<div className='w-1/6 px-3' key={index}>
 								<p className='font-medium text-base mb-3'>{item.name}</p>
 								<ul className='text-xs leading-loose font-thin'>
 									{item.items.map((footerItem, footerIndex) => (
@@ -41,12 +41,22 @@ const Footer = () => {
 								<FontAwesomeIcon icon={faAt} size='sm' /> <span className='ml-2'>contact@kabayanestate.com</span>
 							</div>
 							<div>
-								<FontAwesomeIcon icon={faFacebook} size='2x' className='inline-block mr-5' />
-								<FontAwesomeIcon icon={faTwitter} size='2x' className='inline-block mr-5' />
-								<FontAwesomeIcon icon={faInstagram} size='2x' className='inline-block mr-5' />
-								<FontAwesomeIcon icon={faRss} size='2x' className='inline-block mr-5' />
+								<FontAwesomeIcon icon={faFacebook} className='inline-block mr-4 text-xl' />
+								<FontAwesomeIcon icon={faTwitter} className='inline-block mr-4 text-xl' />
+								<FontAwesomeIcon icon={faInstagram} className='inline-block mr-4 text-xl' />
+								<FontAwesomeIcon icon={faRss} className='inline-block mr-4 text-xl' />
 							</div>
 						</div>
+					</div>
+				</div>
+			</div>
+			<div className='bg-secondary-dark text-gray-300 text-sm flex items-center border-b-4 border-primary'>
+				<div className='p-3 container mx-auto'>
+					<div className='flex items-center'>
+						<svg className='fill-current h-8 w-8 mr-2' width='40' height='40' viewBox='0 0 54 54' xmlns='http://www.w3.org/2000/svg'>
+							<path d='M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z' />
+						</svg>
+						<span className='text-lg'>Kabahay</span>
 					</div>
 				</div>
 			</div>
