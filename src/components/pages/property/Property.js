ae2aaa224381e9ce8f65f5eb6c2523f6c6207d82
Milfren <nerflimjohn@gmail.com>
2019-09-29 23:38:30 +1000

    added property details

--- src/components/pages/property/Property.js
+++ src/components/pages/property/Property.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faBed, faBath, faCarSide, faChartArea } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Agent from './Agent';
+import PropertyItem from '../../PropertyItem';
 
 const Property = props => {
 	const information = {
@@ -56,25 +57,48 @@ const Property = props => {
 			</div>
 			<div className='container mx-auto flex'>
 				<div className='w-2/3 p-3 text-sm'>
-					<h3 className='text-xl mb-2'>Property Description</h3>
-					<p className='mb-3'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula eget nisi nec molestie. Maecenas sed libero non risus pharetra
-						aliquet. In fringilla id tortor porta maximus. Pellentesque dictum neque eget mi dignissim, a mattis velit commodo. Etiam a orci ac odio
-						gravida gravida ut eu arcu. Ut augue odio, semper id nunc a, euismod cursus augue. Mauris a nisl et sem placerat ornare. Vivamus tristique
-						convallis neque at blandit.
-					</p>
-					<p className='mb-3'>
-						Maecenas nec nulla mi. Nulla odio nunc, euismod nec eleifend vehicula, tristique eu est. Nullam cursus massa eros, eu pellentesque est
-						sodales ut. Aenean at risus eu leo dictum ullamcorper. Praesent ac urna ac nisl congue condimentum eu vitae velit. Nunc molestie et ipsum
-						eleifend tempus. Cras ullamcorper aliquam nisi eu tempor. Aliquam erat volutpat. Nullam vulputate dui finibus fermentum blandit. Ut a
-						ligula dictum sapien cursus consequat porta quis ligula. Nulla mattis pretium dapibus. Quisque eu dignissim orci, in tristique augue.
-					</p>
-					<p className='mb-3'>
-						Donec vitae venenatis mauris. Cras facilisis varius diam a fringilla. Donec elementum nec felis gravida venenatis. Maecenas malesuada
-						dapibus mi, eget placerat urna egestas in. Morbi imperdiet dapibus tortor, eu gravida magna egestas non. Vivamus cursus ligula id odio
-						bibendum elementum. Fusce tincidunt mauris sed erat luctus eleifend. Curabitur vitae aliquam lectus, quis fermentum ante. Proin nulla
-						lacus, ultricies quis purus nec, blandit auctor est.
-					</p>
+					<div className='mb-5'>
+						<h3 className='text-xl mb-2'>Property Description</h3>
+						<p className='mb-3'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula eget nisi nec molestie. Maecenas sed libero non risus pharetra
+							aliquet. In fringilla id tortor porta maximus. Pellentesque dictum neque eget mi dignissim, a mattis velit commodo. Etiam a orci ac odio
+							gravida gravida ut eu arcu. Ut augue odio, semper id nunc a, euismod cursus augue. Mauris a nisl et sem placerat ornare. Vivamus
+							tristique convallis neque at blandit.
+						</p>
+						<p className='mb-3'>
+							Maecenas nec nulla mi. Nulla odio nunc, euismod nec eleifend vehicula, tristique eu est. Nullam cursus massa eros, eu pellentesque est
+							sodales ut. Aenean at risus eu leo dictum ullamcorper. Praesent ac urna ac nisl congue condimentum eu vitae velit. Nunc molestie et
+							ipsum eleifend tempus. Cras ullamcorper aliquam nisi eu tempor. Aliquam erat volutpat. Nullam vulputate dui finibus fermentum blandit.
+							Ut a ligula dictum sapien cursus consequat porta quis ligula. Nulla mattis pretium dapibus. Quisque eu dignissim orci, in tristique
+							augue.
+						</p>
+						<p className='mb-3'>
+							Donec vitae venenatis mauris. Cras facilisis varius diam a fringilla. Donec elementum nec felis gravida venenatis. Maecenas malesuada
+							dapibus mi, eget placerat urna egestas in. Morbi imperdiet dapibus tortor, eu gravida magna egestas non. Vivamus cursus ligula id odio
+							bibendum elementum. Fusce tincidunt mauris sed erat luctus eleifend. Curabitur vitae aliquam lectus, quis fermentum ante. Proin nulla
+							lacus, ultricies quis purus nec, blandit auctor est.
+						</p>
+					</div>
+					<div className='mb-5'>
+						<h3 className='text-xl mb-2'>Make an appointment</h3>
+						<div className='flex'>
+							<div className='h-64 bg-blue-400 rounded-lg flex-1'></div>
+							<div className='flex-1'>
+								<h3 className='text-lg mb-2 text-center'>Select available time</h3>
+							</div>
+						</div>
+					</div>
+					<div className='mb-5'>
+						<h3 className='text-xl mb-2'>Other properties for sale near this area</h3>
+						<div className='flex'>
+							<div className='px-2'>
+								<PropertyItem />
+							</div>
+							<div className='px-2'>
+								<PropertyItem />
+							</div>
+						</div>
+					</div>
 				</div>
 				<div className='w-1/3 p-3'>
 					<Agent agent={information.agent} />
