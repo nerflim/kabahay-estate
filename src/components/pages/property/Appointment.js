a1185139a049c5ce0337c370f13067f9b74a902d
Milfren <nerflimjohn@gmail.com>
2019-10-04 00:13:36 +1000

    active class when selecting appointment time

--- src/components/pages/property/Appointment.js
+++ src/components/pages/property/Appointment.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
 const Appointment = () => {
+	const [activeTime, setActiveTime] = useState('');
 	const availableTime = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM'];
+
+	const timeClass = 'py-1 px-3 text-center w-full border rounded-full hover:shadow border-gray-500 focus:outline-none hover:border-primary';
+	const activeTimeClass = 'py-1 px-3 text-center w-full border rounded-full border-primary bg-white text-primary focus:outline-none';
+
 	return (
 		<div className='mb-8'>
 			<h3 className='text-xl mb-2'>Make an appointment</h3>
@@ -15,9 +20,7 @@ const Appointment = () => {
 					<div className='flex flex-wrap'>
 						{availableTime.map((item, index) => (
 							<div className='w-1/3 px-2 mb-3' key={index}>
-								<button
-									type='button'
-									className='py-1 px-3 text-center w-full border rounded-full hover:shadow border-gray-500 focus:outline-none hover:border-primary'>
+								<button type='button' className={activeTime === item ? activeTimeClass : timeClass} onClick={() => setActiveTime(item)}>
 									{item}
 								</button>
 							</div>
